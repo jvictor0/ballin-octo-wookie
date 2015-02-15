c072d8cf7b0f9f88daffaa48f4f679b3599fd6c9
Joseph Victor <jvictor@stanford.edu>
2015-02-15 13:54:29 -0800

    Made libraryable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = "artrat",
+      version = "1",
+      description = "",
+      author = "JoYo",
+      author_email = "jvictor@stanford.edu",
+      url = "",
+      packages = ['artrat'],
+      package_data = {'package' : ["artrat/*"] },
+      ) 
